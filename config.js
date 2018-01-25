8eda7e86289a93aae2a1a56781618eb6fe23e1ce
Hammy Havoc <hammy@splitanatom.com>
2018-01-25 00:03:33 +0000

    Initial commit

---
+++ config.js
@@ -0,0 +1,21 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'fitness-agency-android',
+		wp_ws_url : 'http://www.myfitnessagency.com/wp-appkit-api/fitness-agency-android',
+		wp_url : 'http://www.myfitnessagency.com',
+		theme : 'q-android',
+		version : '1.0',
+		app_title : 'My Fitness Agency',
+		app_platform : 'android',
+		gmt_offset : 0,
+		debug_mode : 'on',
+		auth_key : 'V6UJH&UXhxBl3Iz!BzJNnIrU5bJcytMUCpjxDzWk#Hb4t40RX#*BUFYiJIQhbbBb',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
